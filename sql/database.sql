77a1279b3c63e83a90e6448fc59e7bcaaaa841b6
Mxt@DEV <maxta.gamer@gmail.com>
2024-07-08 12:18:05 -0300

    update

---
+++ sql/database.sql
@@ -0,0 +1,40 @@
+CREATE TABLE authors (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(250) NOT NULL,
+  "bio" TEXT NOT NULL
+);
+
+CREATE TABLE contact_info (
+  "id" SERIAL PRIMARY KEY,
+  "phone" VARCHAR(20) NOT NULL,
+  "email" VARCHAR(200) NOT NULL,
+  "authorId" INTEGER UNIQUE NOT NULL,
+  FOREIGN KEY ("authorId") REFERENCES authors("id") ON DELETE CASCADE
+);
+
+CREATE TABLE books (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(250) NOT NULL, 
+  "pages" INTEGER NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL,
+  "updatedAt" TIMESTAMP NOT NULL,
+  "authorId" INTEGER,
+  FOREIGN KEY ("authorId") REFERENCES authors("id") ON DELETE SET NULL
+);
+
+
+CREATE TABLE categories (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(250) NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL,
+  "updatedAt" TIMESTAMP NOT NULL
+);
+
+
+CREATE TABLE books_categories (
+  "id" SERIAL PRIMARY KEY,
+  "booksId" INTEGER NOT NULL,
+  "categoriesId" INTEGER NOT NULL,
+  FOREIGN KEY ("booksId") REFERENCES books("id") ON DELETE CASCADE,
+  FOREIGN KEY ("categoriesId") REFERENCES categories("id") ON DELETE CASCADE
+);
---
+++ sql/selectData.sql
@@ -0,0 +1,2 @@
+SELECT * FROM books;
+-- parei em Leitura de todos os livros da categoria "Fantasia".
